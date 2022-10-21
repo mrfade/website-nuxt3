dcb2e626e3fa1806c6678a8c5c7d998d83b1f99f
Enes <enessolak99@gmail.com>
2022-10-21 16:32:24 +0300

    pinia config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
   ],
   css: [
     '~~/assets/css/base.scss'
