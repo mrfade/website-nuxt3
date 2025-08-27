f468c548974380f0b1f8bf7bd3da58cbc8876c7c
Enes Solak <enes@solak.dev>
2025-08-27 18:40:11 +0300

    refactor(pinned-repos): simplify API by returning static JSON data directly

--- server/api/pinned-repos.get.ts
+++ server/api/pinned-repos.get.ts
@@ -1,46 +1,6 @@
-import fs from 'fs'
-import path from 'path'
 import { defineEventHandler } from 'h3'
-// import { $fetch, FetchError } from 'ofetch'
-// import { Redis } from '@upstash/redis'
+import pinnedRepos from '../../data/pinned-repos.json'
 
-// const redis = Redis.fromEnv()
-
-export default defineEventHandler(async (_event: any) => {
-  let repos = []
-
-  // read from file /data/pinned-repos.json asynchronously for serverless compatibility
-  const filePath = path.join(process.cwd(), 'data/pinned-repos.json')
-  try {
-    const data = await fs.promises.readFile(filePath, 'utf8')
-    repos = JSON.parse(data)
-  } catch (error) {
-    // ignore error
-    console.error('Error reading pinned repos file:', error)
-  }
-
-  // const cachePinnedRepos: any[] | null = await redis.get('pinnedRepos')
-
-  // if (cachePinnedRepos === null || cachePinnedRepos.length === 0) {
-  //   const pinnedRepos: any[] = await $fetch('https://gh-pinned-repos.egoist.dev/?username=mrfade', {
-  //     responseType: 'json'
-  //   })
-  //     .catch((_error: FetchError) => {
-  //       throw new Error('Failed to fetch pinned repos', {
-  //         cause: _error
-  //       })
-  //     })
-
-  //   const _repos: string = JSON.stringify(pinnedRepos)
-  //   await redis.set('pinnedRepos', _repos, {
-  //     ex: 60 * 60 // 1 hours
-  //   })
-
-  //   repos = pinnedRepos
-  // }
-
-  // if (repos.length === 0)
-  //   repos = cachePinnedRepos as any[]
-
-  return repos
+export default defineEventHandler(() => {
+  return pinnedRepos
 })
